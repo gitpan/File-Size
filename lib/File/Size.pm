8af8fcea9b29c71ab5f5859a1fbe34ead45e847b
Ofer Shaked <oferpn@yahoo.com>
2006-09-23 22:19:45 +0000

    Import of OFER/File-Size-0.02 from CPAN.
    gitpan-cpan-distribution: File-Size
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         OFER/File-Size-0.02.tar.gz
    gitpan-cpan-author:       OFER
    gitpan-cpan-maturity:     released

--- lib/File/Size.pm
+++ lib/File/Size.pm
@@ -1,84 +1,164 @@
 package File::Size;
 
-use 5.008008;
-use strict;
+use 5.006;
 use warnings;
+use strict;
+use File::Find;
+use Cwd;
+use Number::Bytes::Human qw( format_bytes );
+no warnings 'File::Find';
+our $VERSION = '0.02';
 
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use File::Size ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
-our $VERSION = '0.01';
-
-
-# Preloaded methods go here.
-
-1;
-__END__
-# Below is stub documentation for your module. You'd better edit it!
+=pod
 
 =head1 NAME
 
-File::Size - Perl extension for blah blah blah
+File::Size - Get the size of files and directories
 
 =head1 SYNOPSIS
 
-  use File::Size;
-  blah blah blah
+Get the size for directory /etc/ with the block size of 1024 while following symbolic links:
+
+ my $obj = File::Size->new(
+    dir            => '/etc/',
+    blocksize      => 1024,
+    followsymlinks => 1,
+    humanreadable  => 1
+ );
+ print $obj->getsize(), "\n";
 
 =head1 DESCRIPTION
 
-Stub documentation for File::Size, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+File::Size is used to get the size of files and directories.
 
-Blah blah blah.
+There are 6 methods you can use:
 
-=head2 EXPORT
+=over 6
 
-None by default.
+=item B<new>
 
+ There are 4 optional hash values for the new() method:
 
+=over 4
 
-=head1 SEE ALSO
+=item C<dir>
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+The directory you want the module to get the size for it. Default is current working directory.
 
-If you have a mailing list set up for your module, mention it here.
+=item C<blocksize>
 
-If you have a web site set up for your module, mention it here.
+The blocksize for the output of getsize() method. default is 1 (output in bytes).
 
-=head1 AUTHOR
+=item C<followsymlinks>
 
-Ofer Shaked, E<lt>ofer@(none)E<gt>
+If you want to follow symlinks for directories and files, use this option. The default is not to follow symlinks.
 
-=head1 COPYRIGHT AND LICENSE
+=item C<humanreadable>
 
-Copyright (C) 2006 by Ofer Shaked
+If you want output size in human readable format (e.g. 2048 -> 2.0K), set this option to 1.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.8 or,
-at your option, any later version of Perl 5 you may have available.
+=back
 
+ You don't have to specify any of those options, which means this is okay:
+     print File::Size->new()->getsize(), " bytes\n";
+ This is okay too:
+     print File::Size->new()->setdir( '/etc/' )->setblocksize( 1024**2 )->getsize(), " MB\n";
+
+=item B<setdir>
+
+Used to set (or get - if called without parameters) the directory.
+ Example:
+     $obj->setdir( '/etc/' );
+
+=item B<setblocksize>
+
+Used to set (or get - if called without parameters) the block size.
+ Example:
+     $obj->setblocksize( 1024 );
+
+=item B<setfollowsymlinks>
+
+Used to set if you want to follow symbolic links or not. If called without parmeters, returns the current state.
+ Example:
+     $obj->setfollowsymlinks( 1 );
+
+=item B<sethumanreadable>
+
+Used to set (or get - if called without parameters) if you want human-readable output sizes.
+ Example:
+     $obj->sethumanreadable( 1 );
+
+=item B<getsize>
+
+Used to calculate the total size of the directory. Prints output according to the block size you did or didn't specify.
+
+=back
 
 =cut
+
+my $followsymlinks = 0;
+my $blocksize = 0;
+my $size = 0;
+
+sub new {
+	my $class = shift;	
+	my %options = @_;
+	my $self = \%options;
+	bless( $self, $class );
+	return $self;
+}
+
+sub DESTROY {
+	my $self = shift;
+	undef( $self );
+}
+
+sub setdir {
+	my $self = shift;
+	$$self{ 'dir' } = shift || return $$self{ 'dir' };
+	return $self;
+}
+
+sub setblocksize {
+	my $self = shift;
+	$$self{ 'blocksize' } = shift || return $$self{ 'blocksize' };
+	return $self;
+}
+
+sub setfollowsymlinks {
+	my $self = shift;
+	$$self{ 'followsymlinks' } = shift || return $$self{ 'followsymlinks' };
+	return $self;
+}
+
+sub sethumanreadable {
+	my $self = shift;
+	$$self{ 'humanreadable' } = shift || return $$self{ 'humanreadable' };
+	return $self;
+}
+
+sub getsize {
+	my $self = shift;
+	my %options;
+	if ( $$self{ 'followsymlinks' } ) {
+		%options = (
+			wanted		=> \&findcb,
+			follow		=> 1,	# follow symlinks
+			follow_skip => 2	# skip duplicate links, but don't die doing it
+		);
+	} else {
+		%options = (
+			wanted		=> \&findcb
+		);
+	}
+	my $dir = $$self{ 'dir' } || getcwd();
+	my $blocksize = $$self{ 'blocksize' } || 1;
+	find( \%options, $dir );
+	return $$self{ 'humanreadable' } ? format_bytes( $size ) : sprintf( '%d', $size / $blocksize );
+}
+
+sub _findcb {
+	$size += -s $File::Find::name || 0;
+}
+
+1;
--- t/File-Size.t
+++ t/File-Size.t
@@ -5,8 +5,10 @@
 
 # change 'tests => 1' to 'tests => last_test_to_print';
 
-use Test::More tests => 1;
-BEGIN { use_ok('File::Size') };
+use Test;
+BEGIN { plan tests => 1 };
+use File::Size;
+ok(1); # If we made it this far, we're ok.
 
 #########################
 
